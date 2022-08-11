4556f32c4ad24f4ca028ba815439da22b9472191
Berke <hello@berke.dev>
2022-08-12 01:53:31 +0300

    Add dotenv

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
